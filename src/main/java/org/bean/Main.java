49f7003987e68d6f8e941a1a981ef2d6f4c3c70b
Rinshid <mrinshid52gmail.com>
2025-02-04 16:15:03 +0530

    working of bean

---
+++ src/main/java/org/bean/Main.java
@@ -0,0 +1,14 @@
+package org.bean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class Main {
+    public static void main(String args[])
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");
+        TextEditor te = (TextEditor) context.getBean("textEditor");
+        te.spellCheck();
+    }
+}
+
